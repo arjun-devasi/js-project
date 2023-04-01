996f7af30479cc6cbfd30188bca02a2de05ca81a
arjundevas <arjundevasi2624@gmail.com>
2023-04-01 16:03:34 +0530

    css

---
+++ calculator/Discount-calcu/script.js
@@ -0,0 +1,17 @@
+let priceCost= document.querySelector("#price");
+let discount=document.querySelector("#dicount");
+let saving=document.querySelector("#savedAmt");
+let finalCost=document.querySelector("#finalPrice");
+let reset=document.querySelector(".reset");
+
+priceCost.addEventListener("input",calculate)
+discount.addEventListener("input",calculate)
+
+function calculate(){
+    let originalcost =priceCost.value;
+    let discountValue=discount.value;
+   let savingval=originalcost*discountValue/100;
+   let finalVal=originalcost-savingval;
+   saving.value=savingval;
+   finalCost.value=finalVal;
+}
